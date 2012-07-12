b930fdcac00384f8a07e8464005b2e240f3dc6ea
david hodgetts <david.demainlalune@gmail.com>
2012-07-12 19:26:47 +0200

    CanvasLeft Object refactoring

--- canvas_tutorial.js
+++ canvas_tutorial.js
@@ -3,38 +3,10 @@
 
 var init = function () {
 
-    var canvasLeft = document.getElementById('canvas_left');
-    var canvasRight = document.getElementById('canvas_right');
-
-    var ctxLeft = canvasLeft.getContext('2d');
-    var ctxRight = canvasRight.getContext('2d');
 
 
-    var halfWidth = canvasLeft.width / 2;
-    var halfHeight = canvasLeft.height / 2;
-
-    var bufferOfLines = [];
-
-    var BufferOfLines = function(maxLines)
-    {
-        this.maxLines = maxLines;
-        this.lines = [];
 
 
-        this.push = function(newLine){
-            this.lines.push(newLine);
-            
-
-            if (this.lines.length >= maxLines)
-            {
-                this.lines.shift();
-            }
-        };
-    }
-
-    bufferOfLines = new BufferOfLines(canvasRight.height);
-
-    var rotation = 0;
 
     var clear = function(canvas)
     {
@@ -50,44 +22,114 @@ var init = function () {
         context.restore();
     }
 
+    var CanvasLeft = function(canvas)
+    {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.rotation = 0;
+
+        var halfWidth = this.canvas.width / 2;
+        var halfHeight = this.canvas.height / 2;
+
+        this.draw = function()
+        {
+            clear(this.canvas);
+
+            this.ctx.fillStyle = "rgb(0,0,0)";
+            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+            
+        
+            this.ctx.save();
+            this.ctx.translate(halfWidth, halfHeight);
+            this.ctx.scale(3, 3);
+            this.ctx.rotate(toRadians(this.rotation));
+
+            this.rotation = (this.rotation + 1) % 360;
+            
+
+            this.ctx.fillStyle = "rgb(200,0,0)";
+            this.ctx.fillRect(-25, -25, 50, 50);
+
+
+            this.ctx.restore();
+
+            // image data
+            var imageDataForTopLine = this.ctx.getImageData(0, 0, this.canvas.width, 1);
+            return imageDataForTopLine;
+
+        }
+    }
+
+
+
+
+    var BufferOfLines = function(maxLines)
+    {
+        this.maxLines = maxLines;
+        this.lines = [];
+
+
+        this.push = function(newLine){
+            this.lines.push(newLine);
+        
+            if (this.lines.length >= maxLines)
+            {
+                this.lines.shift();
+            }
+        };
+    }
+
+    var canvasLeft = new CanvasLeft(document.getElementById('canvas_left')),
+        canvasRight = document.getElementById('canvas_right');
+
+
+    var ctxRight = canvasRight.getContext('2d');
+
+    var bufferOfLines = new BufferOfLines(canvasRight.height);
+
+    var rotation = 0;
+
+
+
     var toRadians = function(degrees)
     {
         return  degrees * Math.PI / 180; 
     }
 
-    var draw_left = function()
-    {
+    // var draw_left = function()
+    // {
 
-        clear(canvasLeft);
+    //     clear(canvasLeft);
 
-        ctxLeft.fillStyle = "rgb(0,0,0)";
-        ctxLeft.fillRect(0, 0, canvasLeft.width, canvasLeft.height);
+    //     ctxLeft.fillStyle = "rgb(0,0,0)";
+    //     ctxLeft.fillRect(0, 0, canvasLeft.width, canvasLeft.height);
         
     
-        ctxLeft.save();
-        ctxLeft.translate(halfWidth, halfHeight);
-        ctxLeft.scale(3, 3);
-        ctxLeft.rotate(toRadians(rotation));
+    //     ctxLeft.save();
+    //     ctxLeft.translate(halfWidth, halfHeight);
+    //     ctxLeft.scale(3, 3);
+    //     ctxLeft.rotate(toRadians(rotation));
 
-        rotation = (rotation + 1) % 360;
+    //     rotation = (rotation + 1) % 360;
 
-        ctxLeft.fillStyle = "rgb(200,0,0)";
-        ctxLeft.fillRect(-25, -25, 50, 50);
+    //     ctxLeft.fillStyle = "rgb(200,0,0)";
+    //     ctxLeft.fillRect(-25, -25, 50, 50);
 
 
-        ctxLeft.restore();
+    //     ctxLeft.restore();
 
-        // image data
-        var imageDataForTopLine = ctxLeft.getImageData(0, 0, canvasLeft.width, 1);
-        return imageDataForTopLine;
-    }
+    //     // image data
+    //     var imageDataForTopLine = ctxLeft.getImageData(0, 0, canvasLeft.width, 1);
+    //     return imageDataForTopLine;
+    // }
+
 
    var draw = function()
     {
-        var imageDataForTopLine = draw_left();
+        var imageDataForTopLine = canvasLeft.draw();
         draw_right(imageDataForTopLine.data);
     }
-    
+
    var draw_right = function(pixel_data)
     {
        bufferOfLines.push(pixel_data);
@@ -108,9 +150,6 @@ var init = function () {
 
 
         ctxRight.putImageData(rightImageData, 0, 0);
-
-
-        console.log(rightImageData.data.length);
 
     }
 
