05533d5c4715f5323e14fa627119be4037ea45d2
david hodgetts <david.demainlalune@gmail.com>
2014-12-11 22:57:18 +0100

    add speed parameter to avgEast2

--- public/animations/avgEast2.js
+++ public/animations/avgEast2.js
@@ -1,5 +1,5 @@
-define(["bibs/imageDataUtils"], 
-function(idu){
+define(["bibs/imageDataUtils", "bibs/shapes"], 
+function(idu, shapes){
   return {
       draw: function (context, borders){
           var copyDirections = {
@@ -14,7 +14,7 @@ function(idu){
           var y0 = context.canvas.height * 2 / 3;
           var dy = context.canvas.height / 3;
           var margin = 10;
-          var source = borders[cardinalDirection];
+          var source = borders["east"];
 //DIFFERENT
           
           var avgLineX0 = context.canvas.width - 1;
@@ -22,42 +22,26 @@ function(idu){
           var avgLineX1 = avgLineX0;
           var avgLineY1 = avgLineY0 + dy;
 
-          var fromRectangle = {x: 0, 
-                               y: y0, 
-                               width: context.canvas.width - margin, 
-                               height: dy};
-
+          var sourcePixels = idu.sliceImageData(context, source, y0 ,dy);
+          var avgColorArray = idu.averageColor(sourcePixels);
+          var lineColor = idu.array2CSSColor(avgColorArray);
+          context.fillStyle = lineColor;
           
-          var negativeDirection = copyDirection.x === -1 || copyDirection.y === -1;
-          if(negativeDirection){
-              var vertical = copyDirection.x === 0 ;
-              fromRectangle[vertical ? "y" : "x"] += margin;
-          }
-          var toPoint = {x: fromRectangle.x + copyDirection.x,
-                         y: fromRectangle.y + copyDirection.y};
+          var speed = 5;
+          context.fillRect(context.canvas.width - speed, y0, speed, dy);
 
-          var halfBorder = idu.sliceImageData(context, source, y0 ,dy);
-          var avgColorArray = idu.averageColor(halfBorder);
-          context.strokeStyle = idu.array2CSSColor(avgColorArray);
-          context.beginPath();
-          context.moveTo(avgLineX0, avgLineY0);
-          context.lineTo(avgLineX1, avgLineY1);
-          context.closePath();
-          context.stroke();
-
-          var startPoint = idu.vec2d(fromRectangle.x + fromRectangle.width,
-                                      fromRectangle.y);
-          var depth = fromRectangle.width;
-         console.log("",startPoint); 
-          var breadth = fromRectangle.height;
+          var startPoint = idu.vec2d(context.canvas.width, y0);
+          var depth = context.canvas.width - margin;
+          var breadth = dy;
           var directionVec = idu.vec2d(-copyDirection.x, -copyDirection.y);
           var opts = idu.rectangularPixelFlow(startPoint,
                                               breadth,
                                               depth,
                                               directionVec); 
+         var dspeed = speed - 1;
+          opts.fromRectangle.x +=  dspeed;
+          opts.fromRectangle.width -=  dspeed;
           idu.copyContextPixels(context, opts.fromRectangle, opts.toPoint);
-          //copy image one pixel west
-          //idu.copyContextPixels(context, fromRectangle, toPoint);
       },
       setup: function (context){
       }
