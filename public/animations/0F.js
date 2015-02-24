a29ef989d0f5fb376d2c8d24313a9ea930c71fa3
andreas kundig <andreas.kuendig@gmx.ch>
2015-02-24 23:20:46 +0100

    cleaned up 0F, pixel flow

--- public/animations/0F.js
+++ public/animations/0F.js
@@ -8,71 +8,69 @@ function(idu, shapes){
       draw: function (context, borders){
           
           //bottom left sw
-          var color = idu.averageBorderColor(context,
-                                             "south",
-                                             borders,
-                                             this.depth,
-                                             this.depth + this.breadth),
-              rec = this.rectangle(0, this.breadth, this.breadth, this.depth) ;
-          this.drawFlow(context, color, rec, false, -this.speed);
+          var rec = this.rectangle(0, this.breadth, this.breadth, this.depth),
+              srcPixels = idu.sliceImageData(context, borders["west"],
+                                             this.breadth, this.depth);
+          this.drawFlow(context, srcPixels, rec, true, this.speed);
           
           //top right ne
           rec = this.rectangle(this.depth, 0, this.breadth, this.depth) ;
-          color = idu.averageBorderColor(context,
-                                         "north",
-                                         borders,
-                                         this.depth,
-                                         this.depth + this.breadth);
-          this.drawFlow(context, color, rec, false, this.speed);
+          srcPixels = idu.sliceImageData(context, borders["east"],
+                                         0, this.depth);
+          this.drawFlow(context, srcPixels, rec, true, -this.speed);
 
           //top left nw
           rec = this.rectangle(0, 0, this.depth, this.breadth) ;
-          var srcPixels = context.getImageData(0, this.breadth, this.depth, 1);
-          color = idu.averageColor(srcPixels);
-          this.drawFlow(context, color, rec, false, -this.speed);
+          srcPixels = context.getImageData(0, this.breadth, this.depth, 1);
+          this.drawAvgFlow(context, srcPixels, rec, false, -this.speed);
           
           //bottom right se
           rec = this.rectangle(this.breadth, this.depth,
                                this.depth, this.breadth) ;
           srcPixels = context.getImageData(this.breadth, this.depth - 1 , 
                                            this.depth, 1);
-          color = idu.averageColor(srcPixels);
-          this.drawFlow(context, color, rec, false, this.speed);
+          this.drawAvgFlow(context, srcPixels, rec, false, this.speed);
 
           //middle left
           rec = this.rectangle(this.breadth, this.breadth,
                                this.breadth/2, this.breadth) ;
           srcPixels = context.getImageData(this.breadth -1 , this.breadth, 
                                            1, this.breadth);
-          color = idu.averageColor(srcPixels);
-          this.drawFlow(context, color, rec, true, this.speed - 2);
+          this.drawAvgFlow(context, srcPixels, rec, true, this.speed - 2);
           
           //middle right
           rec = this.rectangle(this.breadth * 1.5, this.breadth,
                                this.breadth/2, this.breadth) ;
           srcPixels = context.getImageData(this.depth +1  , this.breadth, 
                                            1, this.breadth);
-          color = idu.averageColor(srcPixels);
-          this.drawFlow(context, color, rec, true, -this.speed + 2);
+          this.drawAvgFlow(context, srcPixels, rec, true, -this.speed + 2);
       },
       rectangle: function(x, y, width, height){
-          return {x: x, 
-                  y: y, 
-                  width: width, 
-                  height: height} ;
+          return {x: x, y: y, width: width, height: height} ;
       },
-      drawFlow: function(context, color, rectangle, horizontal, speed){
-          var opts = idu.pixelFlowParams(rectangle,
-                                                    horizontal, 
-                                                    speed), 
-              srcRec = opts.changeRectangle;
-          
-          context.fillStyle = color;
-          context.fillRect(srcRec.x, srcRec.y, srcRec.width, srcRec.height);
-
+      drawFlow: function(context, srcPixels, rectangle, horizontal, speed){
+          var opts = idu.pixelFlowParams(rectangle, horizontal, speed); 
+          this.drawPixels(context, opts.changeRectangle, srcPixels, horizontal);
           idu.copyContextPixels(context, opts.copyRectangle, opts.pastePoint);
       },
-
+      drawAvgFlow: function(context, srcPixels, rectangle, horizontal, speed){
+          var opts = idu.pixelFlowParams(rectangle, horizontal, speed); 
+          this.drawAvgColor(context, opts.changeRectangle, srcPixels);
+          idu.copyContextPixels(context, opts.copyRectangle, opts.pastePoint);
+      },
+      drawAvgColor: function(context, chgRec, srcPixels){
+          var color = idu.averageColor(srcPixels);
+          context.fillStyle = color;
+          context.fillRect(chgRec.x, chgRec.y, chgRec.width, chgRec.height);
+      },
+      drawPixels: function(context, chgRec, srcPixels, horizontal){
+          var size = horizontal ? chgRec.width : chgRec.height,
+              d= [0,0];
+          for(var i=0; i<size; i++){
+              d[horizontal? 0 : 1] = i;
+              context.putImageData(srcPixels, chgRec.x + d[0], chgRec.y + d[1]);
+          }
+      },
       setup: function (context){
       }
   };
