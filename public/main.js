c6f98d57de473096366048dfba83ccc2588e7885
andreas kundig <andreas.kuendig@gmx.ch>
2015-04-11 01:13:55 +0200

    request-listener taken out of server.js, remove old testing method

--- public/main.js
+++ public/main.js
@@ -1,33 +1,15 @@
 
 var main = function(net, exquisInit, makeEditorView, makeEditorController){
-    window.load = function(){
-        net.loadAnimations()
-            .then(function(assemblage){
-                var exquis =  exquisInit(assemblage.name, assemblage.canvasAnims);
-                exquis.addEditor(makeEditorView, makeEditorController);
-                // this is only for debugging in the console
-                window.x = exquis;
-            });
-
-    };
     window.onerror = function(message, url, lineNumber){
         //console.log(message +" "+ url +" "+ lineNumber);
     };
-    window.test = function(){
-        require(["tests/clientTest"],
-                function(clientTest){
-                    try{
-                        clientTest.test(net);
-                    }catch(e){
-                        console.error(e.stack);
-                    }
-                },
-                function(err){
-                    console.log(err);
-                    console.log("tests have been disabled");
-                });
-    }; 
-    window.load();
+    net.loadAnimations()
+        .then(function(assemblage){
+            var exquis =  exquisInit(assemblage.name, assemblage.canvasAnims);
+            exquis.addEditor(makeEditorView, makeEditorController);
+            // this is only for debugging in the console
+            window.x = exquis;
+        });
 };
 
 require(["net", "exquis", "editorView", "editorController", "lib/domReady!"], main);
